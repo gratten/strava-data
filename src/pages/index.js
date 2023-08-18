4c9c4c035e2c463d1a1157409f5810689b4fde47
gratten <grattenward@gmail.com>
2023-08-17 21:40:25 -0400

    removed all but map, added dark basemap

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,48 +8,31 @@ import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [33.77196, -84.38370]
 
 export default function Home() {
   return (
-    <Layout>
-      <Head>
-        <title>Next.js Leaflet Starter test from gratten</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <Section>
-        <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter test from gratten
-          </h1>
+  <Map className={styles.homeMap} 
+  width="800" 
+  height="400" 
+  center={DEFAULT_CENTER} 
+  zoom={12} 
+  scrollWheelZoom={true}>
+    {({ TileLayer, Marker, Popup }) => (
+      <>
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+        <Marker position={DEFAULT_CENTER}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </>
+    )}
+  </Map>
 
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </>
-            )}
-          </Map>
-
-          <p className={styles.description}>
-            <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
-
-          <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
-          </p>
-        </Container>
-      </Section>
-    </Layout>
   )
 }
